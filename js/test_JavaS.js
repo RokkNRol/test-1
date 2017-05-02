8dbea2413e088f44cf74a81c026331ddb2d2792e
RokkNRol <himera80@mail.ru>
2017-05-02 23:46:33 +0300

    begin_test_1

--- js/test_JavaS.js
+++ js/test_JavaS.js
@@ -1,5 +1,43 @@
-'use strict';
+/*
+/!*'use strict';
 var age = 25, user = 'rok', name = 'sad';
 age=user;
 alert(age);
-alert(name);
+alert(name);*!/
+
+/!*
+var admin="Вася";
+name=admin;
+alert(name);*!/
+
+var PlanetNameEarth="Земля";
+var visitedUser="Петя";*/
+
+// alert(1/0); //infinity
+//alert("sad"/0); //NaN
+/*
+
+//operatory
+
+var apple = "5";
+var banan = "7";
+alert(apple + banan);*/
+/*
+var apple = "5";
+var banan = "7";
+alert(+apple + +banan);*/
+
+/*a=b=5;
+alert(a);*/
+/*
+
+var a=1;
+a++;
+alert(a);
+var a=1;
+a--;
+alert(a);/**!/
+var a = (5, 6);
+alert(a);*/
+for(var i=0; i<15; ++i);
+alert(i);
